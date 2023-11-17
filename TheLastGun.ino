41e7f0ed29d10557ce0af86db061f394ed8a552a
WoochanKang <146752624+FlyingChan@users.noreply.github.com>
2023-11-17 18:51:28 +0900

    Add files via upload

---
+++ TheLastGun.ino
@@ -0,0 +1,62 @@
+#include <MPU6050.h>
+#include <Mouse.h>
+#include <Wire.h>
+#include <Keyboard.h>
+
+int leftbutton = 4;
+int zerobutton = 8;
+
+#define MPU6050_INT_PIN 7
+#define MPU6050_INT digitalPinToInterrupt(MPU6050_INT_PIN)
+
+
+const int MPU_addr=0x68;  // I2C address of the MPU-6050
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+void setup(){
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+  Serial.begin(9600);
+  Mouse.begin();
+  Keyboard.begin();
+  Serial.println("Mouse Start!!");
+  pinMode(leftbutton, INPUT_PULLUP);
+  pinMode(zerobutton, INPUT_PULLUP);
+}   
+
+void loop(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  
+  int16_t gyroX, gyroZ;
+  int Sensitivity = 300;
+  gyroX = GyX / Sensitivity / 1.1 * -1;
+  gyroZ = GyZ / Sensitivity * -1;
+  int a=5;
+  Mouse.move(gyroZ/a, gyroX/a);
+
+
+
+  if (digitalRead(leftbutton)==0) {
+    Mouse.click(MOUSE_LEFT);
+    delay(500);
+  }
+       
+  if (digitalRead(zerobutton)==0) {
+    Keyboard.write(' ');
+    delay(500);
+  }
+}
+
